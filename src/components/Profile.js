716846da5eaa9905f682b40a4a5b88701fecd7ac
Ernesto Alejandro Nappi <eanappi@gmail.com>
2021-07-20 21:17:02 -0300

    Profile component and show this

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,51 @@
+import { useState } from 'react'
+import { ShowProfile } from './ShowProfile'
+
 export const Profile = () => {
+  const [profile, setProfile] = useState({})
+
+  const handleAddProfile = (evt) => {
+    evt.preventDefault()
+    const thumbnail = evt.target.thumbnail.value
+    const title = evt.target.title.value
+    const nickname = evt.target.nickname.value
+    const description = evt.target.description.value
+
+    if (thumbnail === '' || title === '') return
+
+    setProfile({thumbnail, title, nickname, description})
+  }
+  
   return (
-    <section className="my-10 max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Profile</h2>
-        
-        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+    <>
+      <section className="my-10 max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+          Profile
+        </h2>
+        <form onSubmit={handleAddProfile}>
+          <div className="grid grid-cols-1 gap-6 mt-4">
             <div>
-                <input type="url" placeholder="https://Url-Of-Thumbnail" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+              <input type="url" name="thumbnail" autoComplete="off" placeholder="https://Url-Of-Thumbnail" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
             </div>
             <div>
-                <input type="text" placeholder="NickName" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+              <input type="text" name="title" autoComplete="off" placeholder="title" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
+            </div>
+            <div>
+              <input type="text" name="nickname" autoComplete="off" placeholder="NickName" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" />
             </div>
             <div className="col-span-full">
-                <textarea placeholder="Description" className="block w-full h-40 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"></textarea>
+              <textarea name="description" maxLength="140" placeholder="Description" className="block w-full h-40 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"></textarea>
             </div>
-        </div>
-    </section>
+          </div>
+          <div className="flex justify-end mt-6">
+            <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Add
+            </button>
+          </div>
+        </form>
+      </section>
+      
+      <ShowProfile profile={profile} />
+    </>
   )
 }
--- src/components/ShowProfile.js
+++ src/components/ShowProfile.js
@@ -1,3 +1,19 @@
-export const ShowProfile = () => {
-  return <h1>ShowProfile...</h1>
+export const ShowProfile = ({profile}) => {
+  if (Object.keys(profile).length === 0) return null
+
+  return (
+    <div className="max-w-md px-8 py-4 mx-auto mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+        <div className="flex justify-center -mt-16 md:justify-end">
+            <img className="object-cover w-20 h-20 border-2 border-indigo-500 rounded-full dark:border-indigo-400" alt="Thumbnail" src={profile?.thumbnail} />
+        </div>
+
+        <h2 className="mt-2 text-2xl font-semibold text-gray-800 dark:text-white md:mt-0 md:text-3xl">{profile?.title}</h2>
+
+        <p className="mt-2 text-gray-600 dark:text-gray-200">{profile?.description}</p>
+        
+        <div className="flex justify-end mt-4">
+            <span className="text-xl font-medium text-indigo-500 dark:text-indigo-300">{profile?.nickname}</span>
+        </div>
+    </div>    
+  ) 
 }
