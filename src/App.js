6f4960d0da7019e16d1db74fc992418f3da35a75
Ernesto Alejandro Nappi <eanappi@gmail.com>
2021-07-23 12:22:36 -0300

    Restructure route of pages

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import { Route } from "wouter"
 
 import HomePage from './pages/HomePage'
 import NewPage from './pages/NewPage'
-import EditPage from './pages/EditPage'
-import PreviewPage from './pages/PreviewPage'
 
 export const App = () => {
   return (
     <>
       <Route path="/" component={HomePage} />
       <Route path="/new" component={NewPage} />
-      <Route path="/edit:idProfile" component={EditPage} />
-      <Route path="/preview:idProfile" component={PreviewPage} />
     </>
   )
 }
